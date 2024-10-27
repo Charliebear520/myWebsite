c4f0320a2717a0c3d0095d366232b5903c5591fd
charliebear <50406s97116@gmail.com>
2024-10-27 15:08:27 +0800

    edit qualification page

--- src/components/Qualification/index.jsx
+++ src/components/Qualification/index.jsx
@@ -1,7 +1,55 @@
+import React from "react";
+import { Timeline, ConfigProvider } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGraduationCap,
+  faCalendarAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import educationData from "../../json/education.json";
+import styles from "./qualification.module.css";
+
 export default function Qualification() {
   return (
-    <div>
-      <h1>Qualification</h1>
+    <div className={styles.container}>
+      <h2 className={styles.title}>
+        <FontAwesomeIcon icon={faGraduationCap} className={styles.icon} />
+        Education
+      </h2>
+      <ConfigProvider
+        theme={{
+          components: {
+            Timeline: {
+              itemPaddingBottom: "0px",
+            },
+          },
+          token: {
+            /* here is your global tokens */
+            fontSize: 16,
+          },
+        }}
+      >
+        <Timeline>
+          {educationData.map((edu) => (
+            <Timeline.Item key={edu.id} color="#ff6f61">
+              <div className={styles.timelineContent}>
+                <div>
+                  <h3>{edu.school}</h3>
+                  <p>{edu.department}</p>
+                </div>
+                <div>
+                  <p className={styles.period}>
+                    <FontAwesomeIcon
+                      icon={faCalendarAlt}
+                      className={styles.calendarIcon}
+                    />
+                    {edu.period}
+                  </p>
+                </div>
+              </div>
+            </Timeline.Item>
+          ))}
+        </Timeline>
+      </ConfigProvider>
     </div>
   );
 }
